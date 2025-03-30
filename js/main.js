e4f7d15e2c54a8dfa0782c474fd6bc6462ce6006
Shamsiddin <bakhtiyarovshamsiddin@gmail.com>
2025-03-30 09:04:19 +0500

    Lemonwares

--- js/main.js
+++ js/main.js
@@ -6,3 +6,22 @@ navBurger.addEventListener('click', () => {
     navBurgerLine.classList.toggle('active')
     navMenu.classList.toggle('active')
 })
+
+let modal = document.getElementById('myModal');
+let btn = document.getElementById('openModal');
+let span = document.getElementsByClassName('close')[0];
+let modalContent = document.getElementById('modalContent');
+
+btn.onclick = function () {
+    modal.style.display = 'block';
+}
+
+span.onclick = function () {
+    modal.style.display = 'none';
+}
+
+window.onclick = function () {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+}
